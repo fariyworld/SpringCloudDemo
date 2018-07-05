e034038daa30629f24c65be3f9e478d29895fd30
mace <1525752742@qq.com>
2018-07-05 09:29:56 +0800

    spring cloud zuul 实现路由转发 并配置 filter

---
+++ cloud-zuul-gateway-9099/src/main/java/com/mace/cloud/zuul/SpringCloudZuulApplication.java
@@ -0,0 +1,40 @@
+package com.mace.cloud.zuul;
+
+import com.mace.cloud.api.common.UniversalMenthod;
+import com.mace.cloud.zuul.config.AccessFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * description:
+ * <br />
+ * Created by mace on 16:27 2018/7/4.
+ */
+
+@EnableDiscoveryClient//向 eureka 注册服务
+@EnableZuulProxy//开启Zuul的API网关服务功能
+@SpringBootApplication
+public class SpringCloudZuulApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringCloudZuulApplication.class, args);
+    }
+
+
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters(){
+
+        return UniversalMenthod.fastJsonHttpMessageConverters();
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+
+        return new AccessFilter();
+    }
+}
